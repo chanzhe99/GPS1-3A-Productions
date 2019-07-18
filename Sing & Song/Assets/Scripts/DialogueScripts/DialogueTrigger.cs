78a47e4763e724cd940fc4db8fbfd7be46df5d20
hosiang <hosiang79@hotmail.com>
2019-07-19 00:14:06 +0800

    Reupload Aik Eng Dialogue system

---
+++ Sing & Song/Assets/Scripts/DialogueScripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public ObjectDialogue dialogue;
+    //trigger with button
+    /*public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }*/
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if(hit.CompareTag("Player"))
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            //Destroy(gameObject);
+        }
+    }
+}
