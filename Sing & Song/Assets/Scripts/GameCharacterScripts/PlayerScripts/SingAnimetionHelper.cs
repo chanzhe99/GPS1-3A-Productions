7951dd7ce73a8733157625d853d7359c4688e249
gt8472 <0123706@kdu-online.com>
2019-07-24 02:00:56 +0800

    Walking sound effect problem fixed

---
+++ Sing & Song/Assets/Scripts/GameCharacterScripts/PlayerScripts/SingAnimetionHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingAnimetionHelper : MonoBehaviour
+{
+    private void PlayerMoveSound()
+    {
+        SoundManagerScripts.PlaySound("playerWalking");
+    }//make player move sound
+}
