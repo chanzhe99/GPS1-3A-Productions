308693b2d5c8512d71ebfd63814d7de61febce60
gt8472 <0123706@kdu-online.com>
2019-07-24 01:24:25 +0800

    Revert "Revert "PauseMenu2""
    This reverts commit 32c49ac995117e2cecd9330ae15d67e28e580ff8.

---
+++ Sing & Song/Assets/Scripts/PAUSEMENU.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PAUSEMENU : MonoBehaviour
+{
+    private bool paused = false;
+    [SerializeField] private GameObject pauseMenuPanelGameObject;
+
+    void Update()
+    {        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                Pause();
+                Debug.Log("pause");
+            }
+            else
+            {
+                Resume();
+                Debug.Log("onStarting");
+            }
+            Debug.Log("can detecd the key down");
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuPanelGameObject.SetActive(false);
+        paused = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void Pause()
+    {
+        pauseMenuPanelGameObject.SetActive(true);
+        paused = true;
+        Time.timeScale = 0.0f;
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
+
+   /* public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }*/
+}
