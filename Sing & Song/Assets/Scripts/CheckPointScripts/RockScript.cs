289f0d38a5a5a915419833af194dd9fb822b8d41
chanzhe <chanzhe99@gmail.com>
2019-08-08 17:58:34 +0800

    made rock animations trigger when u go near the fire

---
+++ Sing & Song/Assets/Scripts/CheckPointScripts/RockScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockScript : MonoBehaviour
+{
+    [SerializeField] Animator rockAnimator;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            rockAnimator.SetBool("isSaving", true);
+            print(rockAnimator.GetBool("isSaving"));
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            rockAnimator.SetBool("isSaving", false);
+            print(rockAnimator.GetBool("isSaving"));
+        }
+    }
+}
